74d40ec72b16397be329ac2e86ca21762c2f0100
Jonathan Hartman <j@p4nt5.com>
2012-10-23 23:11:25 +0000

    Need to upgrade the kernel and reboot before installing headers

--- definitions/ubuntu12.04-x86-64-512-nomnibus/postinstall.sh
+++ definitions/ubuntu12.04-x86-64-512-nomnibus/postinstall.sh
@@ -5,7 +5,6 @@ date > /etc/vagrant_box_build_time
 # Apt-install various things necessary for Ruby, guest additions,
 # etc., and remove optional things to trim down the machine.
 apt-get -y update
-apt-get -y dist-upgrade
 apt-get -y install linux-headers-$(uname -r)
 apt-get clean
 
